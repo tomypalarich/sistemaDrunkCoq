848f05f294a74156e6442c9c6c1aa392dd6b6497
tomypalarich <tomaspalarich@gmail.com>
2026-07-01 16:28:30 -0300

    Add files via upload

---
+++ src/index.css
@@ -0,0 +1,13 @@
+@import url("https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;600;700&family=Inter:wght@400;500;600&display=swap");
+
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  font-family: "Inter", sans-serif;
+}
+
+.font-display {
+  font-family: "Playfair Display", serif;
+}
